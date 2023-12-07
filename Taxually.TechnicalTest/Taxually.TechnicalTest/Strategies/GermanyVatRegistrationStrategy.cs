45c0c235a7b0c76a6d2ab42cf5c748577c888e5b
jozsapeter88 <jozsapeter88@gmail.com>
2023-12-07 14:23:47 +0100

    add Germany strategy

---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Strategies/GermanyVatRegistrationStrategy.cs
@@ -0,0 +1,26 @@
+﻿using System.Xml.Serialization;
+using Taxually.TechnicalTest.Controllers;
+using Taxually.TechnicalTest.Interfaces;
+
+namespace Taxually.TechnicalTest.Strategies;
+
+public class GermanyVatRegistrationStrategy : IVatRegistrationStrategy
+{
+    private readonly TaxuallyQueueClient _queueClient;
+
+    public GermanyVatRegistrationStrategy(TaxuallyQueueClient queueClient)
+    {
+        _queueClient = queueClient ?? throw new ArgumentNullException(nameof(queueClient));
+    }
+
+    public async Task RegisterAsync(VatRegistrationRequest request)
+    {
+        using (var stringWriter = new StringWriter())
+        {
+            var serializer = new XmlSerializer(typeof(VatRegistrationRequest));
+            serializer.Serialize(stringWriter, request);
+            var xml = stringWriter.ToString();
+            await _queueClient.EnqueueAsync("vat-registration-xml", xml);
+        }
+    }
+}
