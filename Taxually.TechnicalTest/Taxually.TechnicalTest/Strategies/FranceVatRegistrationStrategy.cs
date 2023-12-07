5c28f40348e5e703f7131b37a98a1f45c35ea2e2
jozsapeter88 <jozsapeter88@gmail.com>
2023-12-07 14:21:46 +0100

    add France strategy

---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Strategies/FranceVatRegistrationStrategy.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using Taxually.TechnicalTest.Controllers;
+using Taxually.TechnicalTest.Interfaces;
+
+namespace Taxually.TechnicalTest.Strategies;
+
+public class FranceVatRegistrationStrategy : IVatRegistrationStrategy
+{
+    private readonly TaxuallyQueueClient _queueClient;
+
+    public FranceVatRegistrationStrategy(TaxuallyQueueClient queueClient)
+    {
+        _queueClient = queueClient ?? throw new ArgumentNullException(nameof(queueClient));
+    }
+
+    public async Task RegisterAsync(VatRegistrationRequest request)
+    {
+        var csvBuilder = new StringBuilder();
+        csvBuilder.AppendLine("CompanyName,CompanyId");
+        csvBuilder.AppendLine($"{request.CompanyName}{request.CompanyId}");
+        var csv = Encoding.UTF8.GetBytes(csvBuilder.ToString());
+        await _queueClient.EnqueueAsync("vat-registration-csv", csv);
+    }
+}
